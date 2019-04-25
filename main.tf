6f628205f8a32863564c05eb891bccfebed9ab39
stoffee <chris@roadkill.org>
2019-04-25 15:49:28 -0700

    added fargate module

--- main.tf
+++ main.tf
@@ -33,3 +33,19 @@ terraform {
   namespace = "${var.namespace}"
   ssh_key_name = "${var.ssh_key_name}"
 }
+
+
+ module "fargate" {
+ # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "modules/vault-cluster"
+  source = "git::https://github.com/stoffee/redis-fargate-demo.git//modules/fargate"
+
+
+  aws_access_key = "${var.aws_access_key}"
+  aws_secret_key = "${var.aws_secret_key}"
+  region = "${var.region}"
+  cluster_id = "${var.cluster_id}"
+  namespace = "${var.namespace}"
+  ssh_key_name = "${var.ssh_key_name}"
+}
