b68339383490a2740ee6e77514545b232af990b4
stoffee <chris@roadkill.org>
2019-04-25 14:58:17 -0700

    working on redis module

--- main.tf
+++ main.tf
@@ -17,15 +17,11 @@ provider "aws" {
 }
 
 
-# module "redis" {
-#   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
-#  # to a specific version of the modules, such as the following example:
-#  # source = "modules/vault-cluster"
-#  source = "git::https://github.com/stoffee/redis-fargate-demo.git//modules/redis"
-#
-#
-#  access_key = "${var.aws_access_key}"
-#  secret_key = "${var.aws_secret_key}"
-#  region = "${var.region}"
-#
-#}
+ module "redis" {
+ # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "modules/vault-cluster"
+  source = "git::https://github.com/stoffee/redis-fargate-demo.git//modules/redis"
+
+
+}
