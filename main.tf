0dd019aac05c54bd55a6786ebf31a3a99a2cb2be
stoffee <chris@roadkill.org>
2019-04-25 14:46:17 -0700

    ha! removed a : that was in the wrong place

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "redis" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "modules/vault-cluster"
-  source = "git::https://github.com:stoffee/redis-fargate-demo.git//modules/redis"
+  source = "git::https://github.com/stoffee/redis-fargate-demo.git//modules/redis"
 
 
   access_key = "${var.aws_access_key}"
